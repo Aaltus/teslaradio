c11788c81dce7812fd980aa8408d34577329c41d
Emilien Boisvert <emilien.boisvert@usherbrooke.ca>
2014-02-19 10:28:14 -0500

    Adding Vuforia-JMonkey samples, modified to work with IntelliJ

---
+++ Sample/JME_And_Vuforia/Chapter5/VuforiaJME/obj/local/armeabi-v7a/objs/VuforiaNative/VuforiaNative.o.d
@@ -0,0 +1,170 @@
+/Volumes/DATAz/Prog/Java_Workplace/ProjetS7S8_TeslaRadio/8553OS_Code/Chapter5/VuforiaJME/obj/local/armeabi-v7a/objs/VuforiaNative/VuforiaNative.o: \
+ /Volumes/DATAz/Prog/Java_Workplace/ProjetS7S8_TeslaRadio/8553OS_Code/Chapter5/VuforiaJME/jni/VuforiaNative.cpp \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/string.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/math.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ /Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/QCAR.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/System.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/CameraDevice.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/NonCopyable.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/VideoMode.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/CameraCalibration.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Vectors.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Renderer.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Matrices.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/State.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Frame.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/VideoBackgroundConfig.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Trackable.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Type.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/TrackableResult.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Tool.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Tracker.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/TrackerManager.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/ImageTracker.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/UpdateCallback.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/DataSet.h \
+ ../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Image.h \
+ /Volumes/DATAz/Prog/Java_Workplace/ProjetS7S8_TeslaRadio/8553OS_Code/Chapter5/VuforiaJME/jni/MathUtils.h
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/QCAR.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/System.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/CameraDevice.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/NonCopyable.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/VideoMode.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/CameraCalibration.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Vectors.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Renderer.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Matrices.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/State.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Frame.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/VideoBackgroundConfig.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Trackable.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Type.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/TrackableResult.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Tool.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Tracker.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/TrackerManager.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/ImageTracker.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/UpdateCallback.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/DataSet.h:
+
+../../../ThirdParty/vuforia-sdk-android-2-8-7/build/include/QCAR/Image.h:
+
+/Volumes/DATAz/Prog/Java_Workplace/ProjetS7S8_TeslaRadio/8553OS_Code/Chapter5/VuforiaJME/jni/MathUtils.h:
