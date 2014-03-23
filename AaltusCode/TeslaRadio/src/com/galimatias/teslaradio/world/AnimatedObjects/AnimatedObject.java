9a93f5c23d42a5e2f2d25520246ac6df22fa6a3c
JCMS <jean-christophe.lavoie@usherbrooke.ca>
2014-03-23 18:42:30 -0400

    TR-147 Added some methods and reworked to the skeleton

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/AnimatedObjects/AnimatedObject.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/AnimatedObjects/AnimatedObject.java
@@ -3,7 +3,12 @@ package com.galimatias.teslaradio.world.AnimatedObjects;
 import com.jme3.scene.Geometry;
 
 /**
+ * AnimatedObjects: And AnimatedObject defines an object contained in a scenario. This can refer to a Mesh
+ * object from Blender
  * Created by jean-christophelavoie on 2014-03-23.
  */
 public abstract class AnimatedObject extends Geometry {
+    //TODO: Define global method of the AnimatedObject here
+
+    //TODO: Study if AnimatedObject should be a node that contains multiple objects itself rather than a geometry
 }
--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/Scenarios/Scenario.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/Scenarios/Scenario.java
@@ -1,12 +1,34 @@
 package com.galimatias.teslaradio.world.Scenarios;
 
 
+import com.galimatias.teslaradio.world.AnimatedObjects.AnimatedObject;
+import com.galimatias.teslaradio.world.ViewState;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
+import com.qualcomm.vuforia.Vec3F;
+
+import java.util.List;
 
 
 /**
+ * Scenario: Defines a Scenario node that will includes multiple objects and
+ * manage interaction between thems. This could be refered as a Scene
  * Created by jean-christophelavoie on 2014-03-23.
  */
 public abstract class Scenario extends Node {
+
+    /**
+     * The list of AnimatedObjects that are static
+     */
+    private List<AnimatedObject> lstStaticObjects;
+    /**
+     * The List of AnimatedObjects that are moveable
+     */
+    private List<AnimatedObject> listMoveableObjects;
+
+    private Vector3f mOrigin;
+
+    private ViewState mViewState;
+
 
 }
