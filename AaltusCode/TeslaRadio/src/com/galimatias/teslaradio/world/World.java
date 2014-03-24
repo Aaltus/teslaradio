2ac68874bace210669fea32b2aa94433739514ad
JCMS <jean-christophe.lavoie@usherbrooke.ca>
2014-03-24 09:32:57 -0400

    TR-147 Added methods to World

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/World.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/World.java
@@ -1,9 +1,36 @@
 package com.galimatias.teslaradio.world;
 
+import com.galimatias.teslaradio.world.Scenarios.Scenario;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 
+import java.util.List;
+
 /**
+ * World: Defines the root node of the 3D World
  * Created by jean-christophelavoie on 2014-03-23.
  */
 public class World extends Node {
+
+    //Private attributes
+    private List<Scenario> lstScenario;
+    private Scenario mScenarioInFocus;
+
+    public World() {
+    }
+
+
+    public void UpdateFocus(Vector3f newVector){
+
+    }
+
+    public void UpdateViewables(){
+
+    }
+
+    public void UpdateScenarioState(){
+
+    }
+
+
 }
