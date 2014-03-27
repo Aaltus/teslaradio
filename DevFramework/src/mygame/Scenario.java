8a13725fe3380255c8880fb039d3c11eaa23927e
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-03-27 09:27:21 -0400

    Adding a development framework for 3D modeling using the jMonkey SDK only.

---
+++ DevFramework/src/mygame/Scenario.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.AnimEventListener;
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author Alexandre Hamel
+ */
+public abstract class Scenario extends Node implements AnimEventListener{
+
+    private final static String TAG = "Scenario";
+
+    protected ViewState mViewState;
+
+    protected Node unmovableObjects = new Node("unmovable");
+
+    protected Node movableObjects = new Node("movable");
+
+    protected AssetManager assetManager;
+
+    public Scenario(AssetManager assetManager)
+    {
+        this.assetManager = assetManager;
+    }
+
+    /**
+     * Methods to load the associated 3D objects with the scenario
+     */
+    protected abstract void loadStaticAnimatedObjects();
+
+    protected abstract void loadMovableAnimatedObjects();
+
+    public abstract void onAnimCycleDone(AnimControl animControl, AnimChannel animChannel, String s);
+
+    public abstract void onAnimChange(AnimControl animControl, AnimChannel animChannel, String s);
+
+}
