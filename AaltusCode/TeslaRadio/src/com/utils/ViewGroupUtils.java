285bfe4552be79543ba9ab232080daf934c7de64
Jean-Christophe Lavoie <jean-christophe.lavoie@usherbooke.ca>
2014-03-26 21:56:20 -0400

    Re-adding TR-144

---
+++ AaltusCode/TeslaRadio/src/com/utils/ViewGroupUtils.java
@@ -0,0 +1,34 @@
+package com.utils;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/*
+ * Simple static function to add,remove or replace a view in a layout
+ */
+public class ViewGroupUtils {
+
+    public static ViewGroup getParent(View view) {
+        return (ViewGroup)view.getParent();
+    }
+
+    public static void removeView(View view) {
+        ViewGroup parent = getParent(view);
+        if(parent != null) {
+            parent.removeView(view);
+        }
+    }
+
+    public static void replaceView(View currentView, View newView) {
+        ViewGroup parent = getParent(currentView);
+        if(parent == null) {
+            return;
+        }
+        final int index = parent.indexOfChild(currentView);
+        removeView(currentView);
+        removeView(newView);
+        parent.addView(newView, index);
+    }
+}
