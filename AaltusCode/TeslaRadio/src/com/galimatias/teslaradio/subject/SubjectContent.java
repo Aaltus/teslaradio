df6227491a7cd3e0d4bc63d0ed3fa285859e7ce5
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-03-28 14:14:07 -0400

    developp: Add debugging log in VuforiaJME and VuforiaJMEActivity

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/subject/SubjectContent.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/subject/SubjectContent.java
@@ -26,11 +26,11 @@ public class SubjectContent {
     static {
         //Jonathan Desmarais: Add 7 static sample items.
         // THIS HERE YOU WILL ADD CATEGORY OF INFORMATIVE MENU AND SUBPAGES
-        addItem(new SubjectItem("1", "Introduction", new int[]{R.layout.informative_info_detail_test2}));
-        addItem(new SubjectItem("2", "Transmission du son", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("3", "Modulation", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("4", "Transmission et antennes", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("5", "Demodulation", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test,R.layout.informative_info_detail_test}));
+        addItem(new SubjectItem("1", "Capture du Son", new int[]{R.layout.informative_info_detail_test2}));
+        addItem(new SubjectItem("2", "Modulation AM", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
+        addItem(new SubjectItem("3", "Modulation FM", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
+        addItem(new SubjectItem("4", "Transmission d'information", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
+        addItem(new SubjectItem("5", "Radio Réceptrice", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test,R.layout.informative_info_detail_test}));
         addItem(new SubjectItem("6", "Reference", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
         addItem(new SubjectItem("7", "Qui sommes nous ?", new int[]{R.layout.informative_info_detail_test2}));
     }
