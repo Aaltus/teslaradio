fae0c43f8d796d7b7c200eb1b047f2ec91c22e93
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-03-30 13:09:59 -0400

    TR-171 Change module title based on locale language and add vertical speed slider

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/subject/SubjectContent.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/subject/SubjectContent.java
@@ -1,7 +1,5 @@
 package com.galimatias.teslaradio.subject;
 
-import com.galimatias.teslaradio.R;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,18 +24,26 @@ public class SubjectContent {
     static {
         //Jonathan Desmarais: Add 7 static sample items.
         // THIS HERE YOU WILL ADD CATEGORY OF INFORMATIVE MENU AND SUBPAGES
-        addItem(new SubjectItem("1", "Capture du Son", new int[]{R.layout.informative_info_detail_test2}));
-        addItem(new SubjectItem("2", "Modulation AM", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("3", "Modulation FM", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("4", "Transmission d'information", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("5", "Radio Réceptrice", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test,R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("6", "Reference", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
-        addItem(new SubjectItem("7", "Qui sommes nous ?", new int[]{R.layout.informative_info_detail_test2}));
+//        addItem(new SubjectItem("1", "Capture du Son", new int[]{R.layout.informative_info_detail_test2}));
+//        addItem(new SubjectItem("2", "Modulation AM", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
+//        addItem(new SubjectItem("3", "Modulation FM", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
+//        addItem(new SubjectItem("4", "Transmission d'information", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
+//        addItem(new SubjectItem("5", "Radio Réceptrice", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test, R.layout.informative_info_detail_test,R.layout.informative_info_detail_test}));
+//        addItem(new SubjectItem("6", "Reference", new int[]{R.layout.informative_info_detail_test, R.layout.informative_info_detail_test}));
+//        addItem(new SubjectItem("7", "Qui sommes nous ?", new int[]{R.layout.informative_info_detail_test2}));
     }
 
-    private static void addItem(SubjectItem item) {
+
+
+    public static void addItem(SubjectItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
+    }
+
+    public static void removeAllItems(){
+
+        ITEMS.clear();
+        ITEM_MAP.clear();
     }
 
     /**
