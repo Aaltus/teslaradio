0eb69615825833075a9a90f90eb3c7f8f081611e
David de Grandpré <d.de.grandpre@usherbrooke.ca>
2014-03-31 15:37:17 -0400

    Repackaging of the dev framework and added the following classes: Math, Signal, SignalType.

---
+++ DevFramework/src/commons/Math.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commons;
+
+/**
+ *
+ * @author David
+ */
+public class Math {
+    
+}
