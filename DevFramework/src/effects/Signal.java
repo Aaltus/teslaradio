c4534d7a2ea7e15c0ea0a69e177452efa6db66bb
David de Grandpré <d.de.grandpre@usherbrooke.ca>
2014-03-31 15:37:17 -0400

    Repackaging of the dev framework and added the following classes: Math, Signal, SignalType.

---
+++ DevFramework/src/effects/Signal.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package effects;
+
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+
+
+
+/**
+ *
+ * @author David
+ */
+public class Signal extends Geometry {
+    
+    public Signal(SignalType type, Vector3f path) {
+        
+    }
+    
+    public void SetPropagationSpeed(){
+        
+    }
+    
+    public void SetRotationSpeed(){
+        
+    }
+    
+    public void SetAmplitude(){
+        
+    }
+    
+    public void SetAmplitudeValueQueue(int[] amplitudeArray) {
+        
+    }
+    
+    public void SetPath(/*bezier*/) {
+        
+    }
+    
+}
