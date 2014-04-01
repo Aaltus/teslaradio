b1a73672b3fa3f2a8db9ec42eabfd64f0bb0d1a9
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-04-01 11:26:20 -0400

    Added a method to initalize some direction vectors and refactored the scenario package to scenarios.

--- DevFramework/src/scenarios/SoundCapture.java
+++ DevFramework/src/scenarios/SoundCapture.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package scenario;
+package scenarios;
 
 import com.jme3.animation.AnimChannel;
 import com.jme3.animation.AnimControl;
@@ -12,11 +12,13 @@ import com.jme3.animation.LoopMode;
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Matrix3f;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import commons.Scenario;
+import java.util.Vector;
 
 /**
  *
@@ -36,8 +38,7 @@ public final class SoundCapture extends Scenario {
     private AnimControl mAnimControl = new AnimControl();
     private AnimChannel mAnimChannel;
 
-    private int nbTrajectories = 100;
-    private Vector3f[] trajectories = new Vector3f[nbTrajectories];
+    private Vector<Vector3f> trajectories = new Vector();
     
     private boolean firstTry = true;
        
@@ -115,36 +116,63 @@ public final class SoundCapture extends Scenario {
       
     public void initTrajectories(float nbDirections)
     {
-        /**
-         * Get the position of the drum and microphone
-         */
-        Spatial drum = scene.getParent().getChild("Tambour");
-        Spatial boule_micro = scene.getParent().getChild("Boule_micro");
-        
-        Vector3f drumPosition = drum.getLocalTranslation();
-        Vector3f microPosition = boule_micro.getLocalTranslation();
-        
-        Vector3f drum2MicDirection = microPosition.subtract(drumPosition);
-        drum2MicDirection.normalize();
-       
-        float angleHeight = drum2MicDirection.angleBetween(new Vector3f(drumPosition.x, 0.0f, drumPosition.z));
-        float angleWidth = drum2MicDirection.angleBetween(new Vector3f(drumPosition.x, drumPosition.y, 0.0f));
+//        /**
+//         * Get the position of the drum and microphone
+//         */
+//        Spatial drum = scene.getParent().getChild("Tambour");
+//        Spatial boule_micro = scene.getParent().getChild("Boule_micro");
+//        
+//        Vector3f drumPosition = drum.getLocalTranslation();
+//        Vector3f microPosition = boule_micro.getLocalTranslation();
+//        
+//        Vector3f drum2MicDirection = microPosition.subtract(drumPosition);
+//        drum2MicDirection.normalize();
+//       
+//        /**
+//         * Find the angles of the microphone-drum vector with X axis and Y axis
+//         */
+//        float angleHeight = drum2MicDirection.angleBetween(new Vector3f(drumPosition.x, 0.0f, drumPosition.z));
+//        float angleWidth = drum2MicDirection.angleBetween(new Vector3f(drumPosition.x, drumPosition.y, 0.0f));
         
         Quaternion rotationHeight = new Quaternion();
         Quaternion rotationWidth = new Quaternion();
+        Quaternion normalRotation = new Quaternion();
+        
+        Matrix3f rotMatrixHeight = new Matrix3f();
+        Matrix3f rotMatrixWidth = new Matrix3f();
+        Matrix3f rotMatrixNormal = new Matrix3f();
+        
+        trajectories.add(Vector3f.UNIT_X);
+        
+        Vector3f normalVector = new Vector3f();
+        Vector3f XZPlanVector = new Vector3f();
         
         for(int i=0; i < 20; i++)
-        {
+        {                       
+            rotationWidth.fromAngleAxis(i*(360.0f/20.0f)*2.0f*3.14f, Vector3f.UNIT_Y);
+            rotMatrixWidth = rotationWidth.toRotationMatrix();
+            XZPlanVector = rotMatrixWidth.mult(trajectories.elementAt(i*5));
+            
+            normalRotation.fromAngleAxis(3.14f/2.0f, Vector3f.UNIT_Y);            
+            rotMatrixNormal = normalRotation.toRotationMatrix();
+            normalVector = rotMatrixNormal.mult(trajectories.elementAt(i*5));
+                        
             for(int j=0; j < 5; j++)
-            {   
-                rotationHeight.fromAngleAxis(j*(90.0f/5.0f), trajectories[(i*5)-5]);
-                rotationWidth.fromAngleAxis(i*angleWidth, Vector3f.UNIT_Y);
-                trajectories[i*(j+1)] = Vector3f.ZERO;
+            {                  
+                rotationHeight.fromAngleAxis(j*(90.0f/5.0f)*2.0f*3.14f, normalVector);
+
+                rotMatrixHeight = rotationHeight.toRotationMatrix();
+                
+                XZPlanVector.y = 0;
+                trajectories.add(rotMatrixHeight.mult(XZPlanVector));
+                
             }
         }
+        
+        trajectories.remove(0);
     }
     
-    public void tambourTouchEffect()
+    public void drumTouchEffect()
     {
         movableObjects.attachChild(circles);
         
@@ -160,8 +188,7 @@ public final class SoundCapture extends Scenario {
         mAnimChannel.setSpeed(20.0f);
               
         // Not the first time the object is touched
-        firstTry = false;
-        
+        firstTry = false;        
     }
     
     @Override
