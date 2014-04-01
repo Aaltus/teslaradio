2a83d371f5eaca40531f077d5376c58124da9582
David de Grandpré <d.de.grandpre@usherbrooke.ca>
2014-04-01 11:26:55 -0400

    Added a SignalEmitter and implemented some functions in the Signal object.

--- DevFramework/src/commons/Math.java
+++ DevFramework/src/commons/Math.java
@@ -4,10 +4,24 @@
  */
 package commons;
 
+import com.jme3.math.Vector3f;
+
 /**
  *
  * @author David
  */
 public class Math {
+    
+    public static float SpeedToDurationAlongVector(float speed, Vector3f vector) {
+        return speed/vector.length();
+    }
+    
+    public static Vector3f VectorToUnitary(Vector3f vector) {
+        Vector3f unitary = new Vector3f();
+        unitary.x = vector.x/vector.length();
+        unitary.y = vector.y/vector.length();
+        unitary.z = vector.z/vector.length();
+        return unitary;
+    }
     
 }
