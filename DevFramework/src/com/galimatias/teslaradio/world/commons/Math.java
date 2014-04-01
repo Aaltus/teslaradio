fe57fe7362ba10699e201e7988d55673cb01e0f0
David de Grandpré <d.de.grandpre@usherbrooke.ca>
2014-04-01 15:06:39 -0400

    TR-173 Repackaged the classes in effects and commons

--- DevFramework/src/com/galimatias/teslaradio/world/commons/Math.java
+++ DevFramework/src/com/galimatias/teslaradio/world/commons/Math.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package commons;
+package com.galimatias.teslaradio.world.commons;
 
 import com.jme3.math.Vector3f;
 
