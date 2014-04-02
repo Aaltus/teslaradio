a550d7d7ad9b4f5d7df7414483e38722038c6fe3
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-04-02 15:47:54 -0400

    TR-173 Some changes...

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/Signal.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/Signal.java
@@ -6,62 +6,69 @@ package com.galimatias.teslaradio.world.effects;
 
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Sphere;
-
-
 
 /**
  *
  * @author David
  */
 public class Signal extends Geometry {
-    
+
     private Vector3f path;
+    private Vector3f curvedPath;
     private Vector3f startPoint;
     private float speed;
     private float distanceTraveled;
-    
+    private SignalEmitter emitter;
+
     public Signal(Geometry particle, Vector3f path, float speed) {
-            this.setMesh(particle.getMesh());
-            this.setMaterial(particle.getMaterial());
-            this.speed = speed;
-            this.path = path;
+        this.setMesh(particle.getMesh());
+        this.setMaterial(particle.getMaterial());
+        this.speed = speed;
+        this.path = path;
     }
-    
+
+    public Signal(Geometry particle, Vector3f path)
+    {
+        this.setMesh(particle.getMesh());
+        this.setMaterial(particle.getMaterial());
+        this.curvedPath = path;
+    }
+
     public void updatePosition(float tpf) {
-        
+
         Vector3f currentPos = this.getLocalTranslation();
         float displacement = tpf*speed;
         Vector3f newPos = currentPos.add(path.divide(path.length()).mult(displacement));
         distanceTraveled += displacement;
-        
+
         //Deletion of the object if its at the end of its path.
         if (distanceTraveled>path.length()) {
-            this.setCullHint(CullHint.Always);
+            //  this.setCullHint(CullHint.Always);
+            this.removeFromParent();
         }
         else {
             this.setLocalTranslation(newPos);
         }
     }
-    
+
     public void SetPropagationSpeed(){
-        
+
     }
-    
+
     public void SetRotationSpeed(){
-        
+
     }
-    
+
     public void SetAmplitude(){
-        
+
     }
-    
+
     public void SetAmplitudeValueQueue(int[] amplitudeArray) {
-        
+
     }
-    
+
     public void SetPath(/*bezier*/) {
-        
+
     }
-    
+
 }
