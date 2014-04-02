8ed6aa83c469c27e358c9a6c753f3373b859b764
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-04-01 20:03:30 -0400

    TR-173 Pushing the diffs into the Android code application

---
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/SignalType.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.galimatias.teslaradio.world.effects;
+
+/**
+ *
+ * @author David
+ */
+public enum SignalType {
+    Sound, Modulated1, Modulated2, Modulated3;
+}
