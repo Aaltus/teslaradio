65d1acbc97fd83fa4fb87759bb7822c9e348c01a
David de Grandpré <d.de.grandpre@usherbrooke.ca>
2014-04-03 16:13:48 -0400

    -Adjusted the signal trajectories generator for the main beam -Added an alpha value for secondary signals -Added in and out handles for the drum and the mic

--- DevFramework/src/com/galimatias/teslaradio/world/effects/SignalEmitter.java
+++ DevFramework/src/com/galimatias/teslaradio/world/effects/SignalEmitter.java
@@ -4,14 +4,12 @@
  */
 package com.galimatias.teslaradio.world.effects;
 
-import com.jme3.asset.AssetManager;
-import com.jme3.input.controls.ActionListener;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Vector;
 
 /**
@@ -23,13 +21,14 @@ public class SignalEmitter extends Node{
     private Vector<Vector3f> paths = new Vector<Vector3f>();
     private Geometry particle;
     private float particlesSpeed;
+    private ColorRGBA baseColor;
     
     
-    public SignalEmitter(Vector<Vector3f> paths, Geometry particle, float particlesSpeed) {
+    public SignalEmitter(Vector<Vector3f> paths, Geometry particle, float particlesSpeed, ColorRGBA baseColor) {
         this.paths = paths;
         this.particle = particle;
         this.particlesSpeed = particlesSpeed;
-        
+        this.baseColor = baseColor;
     }
     
     public void simpleUpdate(float tpf) {
@@ -43,8 +42,32 @@ public class SignalEmitter extends Node{
     }
 
     public void emitParticles() {
+        boolean setTransparency=false;
+        
+        Geometry translucentParticle = particle.clone();
+        
+        
+        ColorRGBA translucentColor = new ColorRGBA();
+        translucentColor.r=baseColor.r;
+        translucentColor.g=baseColor.g;
+        translucentColor.b=baseColor.b;
+        translucentColor.a=0.15f;
+        
+        translucentParticle.getMaterial().setColor("Color", translucentColor);
+        
+        baseColor.a=1.0f;
+        
         for (Vector3f path : paths) {
-            Signal mySignal = new Signal(particle, path, particlesSpeed);
+            
+            Signal mySignal;
+            int a = paths.indexOf(path);
+            System.out.println(a);
+            if (paths.indexOf(path)==0)
+                mySignal = new Signal(particle, path, particlesSpeed, baseColor);
+            else {
+                mySignal = new Signal(translucentParticle, path, particlesSpeed, translucentColor);
+            }
+            
             this.attachChild(mySignal);
         }
     }    
