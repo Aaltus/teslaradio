cb28cfd4a5825e50065d8beee69ecc944371eaab
David de Grandpré <d.de.grandpre@usherbrooke.ca>
2014-04-03 16:13:48 -0400

    -Adjusted the signal trajectories generator for the main beam -Added an alpha value for secondary signals -Added in and out handles for the drum and the mic

--- DevFramework/src/com/galimatias/teslaradio/world/effects/Signal.java
+++ DevFramework/src/com/galimatias/teslaradio/world/effects/Signal.java
@@ -4,8 +4,15 @@
  */
 package com.galimatias.teslaradio.world.effects;
 
+import com.jme3.material.RenderState;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
+import java.util.Vector;
+
+
 
 /**
  *
@@ -14,28 +21,42 @@ import com.jme3.scene.Geometry;
 public class Signal extends Geometry {
     
     private Vector3f path;
-    private Vector3f curvedPath;
+    
+    private Vector<Vector3f> curvedPath = new Vector<Vector3f>();
+    private boolean isCurved;
+    
     private Vector3f startPoint;
     private float speed;
     private float distanceTraveled;
-    private SignalEmitter emitter;
     
-    public Signal(Geometry particle, Vector3f path, float speed) {
+    public Signal(Geometry particle, Vector3f path, float speed, ColorRGBA baseColor) {
             this.setMesh(particle.getMesh());
             this.setMaterial(particle.getMaterial());
             this.speed = speed;
             this.path = path;
+            this.isCurved = false;
+            this.getMaterial().setColor("Color", baseColor);
+            this.getMaterial().getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
+            this.setQueueBucket(Bucket.Translucent);
     }
     
-    public Signal(Geometry particle, Vector3f path)
+    public Signal(Geometry particle, Vector<Vector3f> curvedPath, float speed)
     {
         this.setMesh(particle.getMesh());
         this.setMaterial(particle.getMaterial());
-        this.curvedPath = path;
+        curvedPath.equals(curvedPath);
+        this.speed = speed;
+        this.isCurved = true;
     }
     
-    public void updatePosition(float tpf) {
+    private void updateCurvedPosition(float tpf)
+    {
         
+        
+    }
+    private void updateLinearPosition(float tpf)
+    {
+            
         Vector3f currentPos = this.getLocalTranslation();
         float displacement = tpf*speed;
         Vector3f newPos = currentPos.add(path.divide(path.length()).mult(displacement));
@@ -47,7 +68,23 @@ public class Signal extends Geometry {
             this.removeFromParent();
         }
         else {
+            // set scaling
+            this.setLocalScale(1-(distanceTraveled/path.length()));
+            // set position
             this.setLocalTranslation(newPos);
+        }    
+        
+    }
+    
+    public void updatePosition(float tpf) {
+
+        if(isCurved)
+        {
+            updateCurvedPosition(tpf);
+        }
+        else
+        {
+            updateLinearPosition(tpf);
         }
     }
     
