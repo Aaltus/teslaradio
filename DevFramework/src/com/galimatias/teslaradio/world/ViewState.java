e6443d2ddf7d1e103fdb391b18c345d02d2d441a
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-04-08 11:27:28 -0400

    TR-173 Delete DevFramework to avoid confusion.

--- DevFramework/src/com/galimatias/teslaradio/world/ViewState.java
+++
@@ -1,9 +0,0 @@
-package com.galimatias.teslaradio.world;
-
-/**
- * Created by jean-christophelavoie on 2014-03-23.
- */
-//TODO: Full this enum with the good values
-public enum ViewState {
-    FOCUS, NOT_FOCUS, HIDDEN;
-}
