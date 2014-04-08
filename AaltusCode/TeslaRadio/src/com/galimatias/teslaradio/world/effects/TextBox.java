12dd0b0908d64cd51e93b51acfa156e29ebba0e5
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-04-08 17:27:22 -0400

    TR-173 Added a transparent box behind the floating text.

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/TextBox.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/TextBox.java
@@ -3,12 +3,16 @@ package com.galimatias.teslaradio.world.effects;
 import com.jme3.asset.AssetManager;
 import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
-import com.jme3.font.Rectangle;
+import com.jme3.material.Material;
+import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
 
 /**
  * Created by Alexandre Hamel on 4/8/14.
@@ -17,9 +21,11 @@ public class TextBox extends Node {
 
     private BitmapText text;
     private BitmapFont guiFont;
+    private AssetManager assetManager;
 
     public TextBox(AssetManager assetManager)
     {
+        this.assetManager = assetManager;
         this.detachAllChildren();
         guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
         text = new BitmapText(guiFont, false);
@@ -33,7 +39,7 @@ public class TextBox extends Node {
         text.setSize(updatedSize);
         text.setText(updatedText);
         text.setColor(updatedColor);
-        text.lookAt(cam.getLocation(), cam.getUp());
+        this.lookAt(cam.getLocation(), cam.getUp());
     }
 
     public void initText(String textToDisplay,
@@ -44,16 +50,28 @@ public class TextBox extends Node {
     {
         text.setSize(size);
         text.setText(textToDisplay);
-
+        
         float width = text.getLineWidth();
         float height = text.getLineHeight();
-        Rectangle rect = new Rectangle(0.0f, size, width, height);
-        text.setBox(rect);
+                
+        float boxDepth = 0.5f;
+        Box rect = new Box(width-10.0f, height-5.0f, boxDepth);
+        Geometry geomRect = new Geometry("textBox", rect);
+        geomRect.move(width / 2 + 5.0f, -height / 2, -2 * boxDepth - 20.0f);
 
+        Material boxMat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
+        boxMat.setColor("Color", new ColorRGBA((float)0x21/0xFF,(float)0x21/0xFF,(float)0x21/0xFF,(float)0xCC/0xFF));
+        boxMat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
+        geomRect.setMaterial(boxMat);
+
+        text.setQueueBucket(Bucket.Transparent);
+        geomRect.setQueueBucket(Bucket.Transparent);
+        
         this.move(translation);
         this.rotate(rotation);
         text.setColor(color);
-        text.setAlpha(0.5f);
+
+        this.attachChild(geomRect);
         this.attachChild(text);
     }
 }
