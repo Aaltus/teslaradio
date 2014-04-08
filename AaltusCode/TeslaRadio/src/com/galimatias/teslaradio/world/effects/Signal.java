8b54449d32c76612dcb64633c6fb33926ede70fd
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-04-08 12:01:31 -0400

    TR-173: Update Observer package name

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/Signal.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/Signal.java
@@ -5,11 +5,10 @@
 package com.galimatias.teslaradio.world.effects;
 
 import com.jme3.material.RenderState;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
+
 import java.util.Vector;
 
 
