7d76fa7a6412ca56e1e0f51edc9124c1ca59221e
David de Grandpré <d.de.grandpre@usherbrooke.ca>
2014-04-10 14:13:07 -0400

    TR-173 Added impulse response support for the emitters and modified the observer pattern to comply and connect the wire.

---
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/observer/EmitterObserver.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.galimatias.teslaradio.world.observer;
+
+/**
+ *
+ * @author David
+ */
+public interface EmitterObserver {
+    public void observerUpdate(float magnitude);
+}
