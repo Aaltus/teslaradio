22d7a910c05debec9e0f6e44ec891919f4cdf160
Jean-Christophe Lavoie <jean-christophe.lavoie@usherbooke.ca>
2014-04-16 16:10:10 -0400

    TR-182 Dual tracking is working. Origin shifting is so so

--- AaltusCode/TeslaRadio/jni/World.h
+++ AaltusCode/TeslaRadio/jni/World.h
@@ -10,25 +10,34 @@
 
 #include <map>
 #include <list>
-namespace aaltus{
-class World{
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+typedef struct CWorld {} CWorld;
+
+CWorld* CinitWorld();
+int CinitTracker(CWorld* cworld);
+int CdeInitTracker(CWorld* cworld);
+int CloadTrackers(CWorld* cworld);
+int CdestroyTrackerData(CWorld* cworld);
+
+#ifdef __cplusplus
+}
+#endif
+
+class World: public CWorld{
 public:
-	World();
-	virtual ~World();
-
-	int initializeTracker();
-	int deInitTracker();
-	int loadTrackers();
-	void destroyTrackerData();
-	AaltusTrackable* getTrackable(const char* name);
-	AaltusTrackable* getTrackable(std::string name);
-	
-	void			 setOrigin(const char* name);
-	
-	
+    World();
+    ~World();
+    int initializeTracker();
+    int deInitTracker();
+    int loadTrackers();
+    void destroyTrackerData();
+    void setOrigin(const char* name);
+    AaltusTrackable* getTrackable(const char* name);
+    AaltusTrackable* getTrackable(std::string name);
 private:
-	std::map<std::string, AaltusTrackable> _aaltusMap;
-
-	
+    std::map<std::string, AaltusTrackable> _aaltusMap;
 };
-}//namespace aaltus
