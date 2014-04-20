f9147902f36f7f8c8778104ffad94a19b14199e1
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-04-20 11:03:39 -0400

    TR-173 Add comments to the codes (again)

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/ViewState.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/ViewState.java
@@ -4,6 +4,7 @@ package com.galimatias.teslaradio.world;
  * Created by jean-christophelavoie on 2014-03-23.
  */
 //TODO: Full this enum with the good values
-public enum ViewState {
+public enum ViewState
+{
     FOCUS, NOT_FOCUS, HIDDEN;
 }
