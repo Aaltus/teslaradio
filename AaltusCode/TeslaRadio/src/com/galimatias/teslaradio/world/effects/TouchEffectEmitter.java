7e0b30d5cecdd8c9d3263c8b51b398ba60df767d
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-04-20 20:59:53 -0400

    TR-173 Added the classes for touch effects on both the drum or any other object. Not working as for right now but doesn't break anything so I commited...

---
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/effects/TouchEffectEmitter.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.galimatias.teslaradio.world.effects;
+
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class TouchEffectEmitter extends Node {
+    
+    private boolean isTouched = false;
+    private float baseScale;
+    private float maxScale;
+    private float scaleGradient;
+    private Vector3f path;
+    private Geometry effectToScale;
+       
+    public TouchEffectEmitter(String name, float baseScale, float maxScale, float scaleGradient, Geometry effectToScale, Vector3f path) {
+        
+        this.setName(name);
+        this.path = path;
+        this.baseScale = baseScale;
+        this.maxScale = maxScale;
+        this.scaleGradient = scaleGradient;
+        this.effectToScale = effectToScale;
+    }
+    
+    public void isTouched() {
+        
+        this.isTouched = true;
+    }
+    
+    public void simpleUpdate(float tpf) {
+        
+        TouchEffect effect;
+        
+        for (Spatial scaledGeo : this.getChildren()) {            
+            effect = (TouchEffect)scaledGeo;
+            effect.updateScaling(tpf);
+        }
+        
+        if(isTouched) {
+                     
+            TouchEffect myEffect = new TouchEffect(scaleGradient, baseScale, maxScale, path, effectToScale);
+            this.attachChild(myEffect);
+            isTouched = false;
+        }
+    }
+    
+}
