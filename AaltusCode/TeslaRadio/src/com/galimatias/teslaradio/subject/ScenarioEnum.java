d6e3daff4e5e4615e7bb4784a073bf296607a1e5
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-04-30 00:23:06 -0400

    develop: Remove appcompat unused library and continue clean up

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/subject/ScenarioEnum.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/subject/ScenarioEnum.java
@@ -7,6 +7,6 @@ package com.galimatias.teslaradio.subject;
 public enum ScenarioEnum
 {
         SOUNDCAPTURE, AMMODULATION, FMMODULATION, TRANSMIT,
-        RECEPTION, REFERENCE, ABOUTUS;
+        RECEPTION, REFERENCE, ABOUTUS
 
 }
