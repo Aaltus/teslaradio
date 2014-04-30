cfa6e9fe1c09e659cded00252ac6e4d8973ea63b
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-04-30 13:25:27 -0400

    develop: Adjust crash report to only send good info.

--- AaltusCode/TeslaRadio/src/com/utils/ACRAApplication.java
+++ AaltusCode/TeslaRadio/src/com/utils/ACRAApplication.java
@@ -20,14 +20,15 @@ import org.acra.annotation.ReportsCrashes;
 @ReportsCrashes(formKey="dGVacG0ydVHnaNHjRjVTUTEtb3FPWGc6MQ",
         mode = ReportingInteractionMode.DIALOG,
         mailTo = "dms.jonathan@gmail.com",
-        customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.BRAND, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT },
-        logcatArguments = { "-t", "400", "-v", "time" },
+        customReportContent = {ReportField.USER_COMMENT, ReportField.USER_CRASH_DATE , ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.BRAND, ReportField.AVAILABLE_MEM_SIZE, ReportField.TOTAL_MEM_SIZE, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT },
+        logcatArguments = { "-t", "100", "-v", "time" },
         resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
         resDialogText = R.string.crash_dialog_text,
         resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
         resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
         resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
         resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+
 )
 public class ACRAApplication extends Application {
     @Override
