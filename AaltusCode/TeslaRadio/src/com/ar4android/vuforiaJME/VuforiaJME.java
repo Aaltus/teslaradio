622323cb95e11cb8ca018d5edf4c873ca15f47aa
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-09-02 19:44:59 -0400

    Tracking of the trackable has been updated. The camera is now our reference in the world and all our objects are now moving around it. This will help us in doing multiple target tracking.

--- AaltusCode/TeslaRadio/src/com/ar4android/vuforiaJME/VuforiaJME.java
+++ AaltusCode/TeslaRadio/src/com/ar4android/vuforiaJME/VuforiaJME.java
@@ -61,6 +61,8 @@ public class VuforiaJME extends SimpleApplication  implements TouchListener{
 	private boolean mVideoImageInitialized = false;
 	// A flag indicating if a new Android camera image is available.
 	boolean mNewCameraFrameAvailable = false;
+    // The rotation matrix of the world
+    private com.jme3.math.Matrix3f rotMatrix;
 
 
 
@@ -351,42 +353,31 @@ public class VuforiaJME extends SimpleApplication  implements TouchListener{
 //         Log.d(TAG, "Coordinates : x = " + Float.toString(cam_x) + " y = "
 //                 + Float.toString(cam_y) + " z = " + Float.toString(cam_z));
 
-         Log.d(TAG, "Sound Capture Position X : " + soundCapture.getWorldTranslation().x);
-         Log.d(TAG, "Sound Capture Position Y : " + soundCapture.getWorldTranslation().y);
-         Log.d(TAG, "Sound Capture Position Z : " + soundCapture.getWorldTranslation().z);
-
-         Log.d(TAG, "RootNode Position X : " + rootNode.getLocalTranslation().x);
-         Log.d(TAG, "RootNode Position Y : " + rootNode.getLocalTranslation().y);
-         Log.d(TAG, "RootNode Position Z : " + rootNode.getLocalTranslation().z);
-
-         Log.d(TAG, "Camera Position X : " + fgCam.getLocation().x);
-         Log.d(TAG, "Camera Position Y : " + fgCam.getLocation().y);
-         Log.d(TAG, "Camera Position Z : " + fgCam.getLocation().z);
-
          // Set the new foreground camera position
          fgCam.setLocation(new Vector3f(0.0f, 0.0f, 0.0f));
 		 rootNode.setLocalTranslation(new Vector3f(-cam_x, -cam_y, cam_z));
 
 	}
-	
+
 	public void setCameraOrientationNative(float cam_right_x,float cam_right_y,float cam_right_z,
 			float cam_up_x,float cam_up_y,float cam_up_z,float cam_dir_x,float cam_dir_y,float cam_dir_z) {
-		 
+
 		//Log.d(TAG,"Update Orientation Pose..");
 
 //        Log.d(TAG, "direction : x = " + Float.toString(cam_dir_x) + " y = "
 //                + Float.toString(cam_dir_y) + " z = " + Float.toString(cam_dir_z));
 
    		 //left,up,direction
-		    fgCam.setAxes(
-                 new Vector3f(1.0f, 0.0f, 0.0f),
-                 new Vector3f(0.0f, 1.0f, 0.0f),
-                 new Vector3f(0.0f, 0.0f, 1.0f));
+		 fgCam.setAxes(
+             new Vector3f(1.0f, 0.0f, 0.0f),
+             new Vector3f(0.0f, 1.0f, 0.0f),
+             new Vector3f(0.0f, 0.0f, 1.0f));
 
         // Adding the world rotation
-        com.jme3.math.Matrix3f rotMatrix = new com.jme3.math.Matrix3f(1.0f, -cam_right_y, cam_right_z,
-                                                                      cam_up_x, cam_up_y, cam_up_z,
-                                                                      cam_dir_x, cam_dir_y, cam_dir_z);
+        rotMatrix = new com.jme3.math.Matrix3f( cam_right_x, cam_up_x , -cam_dir_x ,
+                                                cam_right_y, cam_up_y, -cam_dir_y,
+                                                -cam_right_z , -cam_up_z , cam_dir_z);
+
         rootNode.setLocalRotation(rotMatrix);
 
 	}
