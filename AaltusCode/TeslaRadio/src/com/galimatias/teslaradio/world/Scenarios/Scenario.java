37ff5b9a6d451f7b24f9e09646d858a5b5f3f17c
Jean-Christophe Lavoie <jean-christophe.lavoie@usherbrooke.ca>
2014-10-15 15:50:37 -0400

    Fixed conflict error

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/Scenarios/Scenario.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/world/Scenarios/Scenario.java
@@ -155,7 +155,6 @@ public abstract class Scenario extends Node implements SignalObserver {
      * Initialization of the title boxes of a scenario.
      */
     protected abstract void initTitleBox();
-<<<<<<< HEAD
     
     /**
      * Start the auto generation of particles
@@ -185,7 +184,7 @@ public abstract class Scenario extends Node implements SignalObserver {
         return this.needAutoGenIfMain;
     }
     
-=======
+
 
     /**
      * This method will apply an opposite trackable rotation on the model, preventing it from rotating
@@ -197,7 +196,7 @@ public abstract class Scenario extends Node implements SignalObserver {
         rot.fromAngleAxis(-ZXangle, Vector3f.UNIT_Y);
         scene.setLocalRotation(rot);
     }
->>>>>>> develop
+
     
     
 }
