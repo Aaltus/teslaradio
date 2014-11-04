d1d561a47e5bddaef311a4ba064f58af4c2dd09d
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-11-03 23:10:25 -0500

    develop: Now the speech bubble can be remove or appeared.

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/TutorialFragment.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/TutorialFragment.java
@@ -13,13 +13,16 @@ import android.widget.TextView;
 /**
  * Created by jimbojd72 on 11/3/2014.
  */
-public class TutorialFragment extends Fragment {
+public class TutorialFragment extends Fragment implements View.OnClickListener {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View myView = inflater.inflate(R.layout.tutorial_layout, null, false);
 
         TextView textView = (TextView)myView.findViewById(R.id.tutorial_speech_textview);
+
+        View characterButton     = myView.findViewById(R.id.character_tutorial_button);
+        characterButton.setOnClickListener(this);
         //textView.setText("Love cats.");
 
         //Get the rootView of the activity. This view is on the direct parent
@@ -45,4 +48,26 @@ public class TutorialFragment extends Fragment {
         return myView;
     }
 
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId())
+        {
+            case R.id.character_tutorial_button:
+
+                toggleTextViewVisibility();
+                break;
+
+        }
+    }
+
+    private void toggleTextViewVisibility() {
+        TextView textView = (TextView)getView().findViewById(R.id.tutorial_speech_textview);
+        if(textView.getVisibility() == View.GONE){
+            textView.setVisibility(View.VISIBLE);
+        }
+        else{
+            textView.setVisibility(View.GONE);
+        }
+    }
 }
