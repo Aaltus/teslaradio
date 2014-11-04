047535125656623766abc77a995595827c2643ce
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-11-03 23:25:31 -0500

    develop: THE CAT IS SHAKING !!!

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/TutorialFragment.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/TutorialFragment.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 /**
@@ -22,30 +24,23 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
         TextView textView = (TextView)myView.findViewById(R.id.tutorial_speech_textview);
 
         View characterButton     = myView.findViewById(R.id.character_tutorial_button);
+        Animation shakeAnim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.shake);
+        characterButton.startAnimation(shakeAnim);
+
         characterButton.setOnClickListener(this);
-        //textView.setText("Love cats.");
-
-        //Get the rootView of the activity. This view is on the direct parent
-        //to the android jme opengl view
-
-        //ViewGroup rootView = (ViewGroup) getActivity().findViewById(android.R.id.content);
-
-        //Inflate and add the top level layout to the rootview
-        //LayoutInflater factory = LayoutInflater.from(getActivity());
-
-        //rootView.addView(myView);
-
-        //Setup the ListFragment
-        /*FragmentManager fm = getChildFragmentManager();//sgetSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        Fragment fragment = new ItemListFragment();
-        ft.hide(fragment);
-        ft.replace(R.id.item_list_fragment_vuforia, fragment, ITEM_LIST_FRAGMENT_TAG);
-        ft.commit();
-        fm.executePendingTransactions(); //TO do it quickly instead of waiting for commit()
-        */
 
         return myView;
+    }
+
+    private void setShakeAnimation(boolean enabled) {
+        View characterButton = getView().findViewById(R.id.character_tutorial_button);
+        if(enabled) {
+            Animation shakeAnim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.shake);
+            characterButton.startAnimation(shakeAnim);
+        }
+        else{
+            characterButton.clearAnimation();
+        }
     }
 
     @Override
@@ -65,9 +60,11 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
         TextView textView = (TextView)getView().findViewById(R.id.tutorial_speech_textview);
         if(textView.getVisibility() == View.GONE){
             textView.setVisibility(View.VISIBLE);
+            setShakeAnimation(false);
         }
         else{
             textView.setVisibility(View.GONE);
+            setShakeAnimation(true);
         }
     }
 }
