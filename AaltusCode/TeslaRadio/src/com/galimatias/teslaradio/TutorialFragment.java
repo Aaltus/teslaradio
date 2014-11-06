355a0da4115d442de555c4f326661ad7b28bd20f
Jonathan Desmarais <dms.jonathan@gmail.com>
2014-11-05 23:27:38 -0500

    develop: Add a calm state to the application

--- AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/TutorialFragment.java
+++ AaltusCode/TeslaRadio/src/com/galimatias/teslaradio/TutorialFragment.java
@@ -24,7 +24,45 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
 
     private final static String TAG = TutorialFragment.class.getSimpleName();
 
+    private Animation shakeAnim;
+    private AnimationDrawable speakAnim;
+
+    private enum CharacterAction{
+
+        CALM,
+        SHAKING,
+        SPEAKING
+
+    }
+
+    private void setCharacterAction(CharacterAction characterAction){
+
+        //Clean up current application
+        getCharacterView().clearAnimation();
+
+
+        //setAnimation
+        switch (characterAction){
+
+            case CALM:
+                setCalmAnimation();
+                break;
+
+            case SHAKING:
+                setShakeAnimation();
+                break;
+
+            case SPEAKING:
+                setSpeakAnimation();
+                break;
+
+        }
+
+    }
+
     private ITutorialSwitcher tutorialSwitcher;
+    private boolean wantToSpeak = true;
+
     public void setTutorialSwitcher(ITutorialSwitcher tutorialSwitcher) {
         this.tutorialSwitcher = tutorialSwitcher;
     }
@@ -43,9 +81,12 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
         View bubbleView = myView.findViewById(R.id.bubble_root_view);
         bubbleView.setVisibility(View.GONE);
 
+
         View characterButton     = myView.findViewById(R.id.character_tutorial_button);
-        Animation shakeAnim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.shake);
+        shakeAnim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.shake);
         characterButton.startAnimation(shakeAnim);
+
+        //speakAnim = AnimationUtils.loadAnimation(this.getActivity(),R.drawable.tesla_speak_anim);
 
         View viewFlipper = myView.findViewById(R.id.view_flipper);
 
@@ -55,31 +96,26 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
         return myView;
     }
 
-    private void setShakeAnimation(boolean enabled) {
-        View characterButton = getView().findViewById(R.id.character_tutorial_button);
-        if(enabled) {
-            Animation shakeAnim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.shake);
-            characterButton.startAnimation(shakeAnim);
-        }
-        else{
-            characterButton.clearAnimation();
-        }
+    private ImageView getCharacterView() {
+        return (ImageView)getView().findViewById(R.id.character_tutorial_button);
     }
 
-    private void setSpeakAnimation(boolean enabled) {
-        ImageView characterButton = (ImageView)getView().findViewById(R.id.character_tutorial_button);
-        if(enabled) {
-            //Animation shakeAnim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.shake);
-            //characterButton.startAnimation(shakeAnim);
-            characterButton.setBackgroundResource(R.drawable.tesla_speak_anim);
-            AnimationDrawable animation = (AnimationDrawable) characterButton.getBackground();
-            animation.start();
-        }
-        else{
-            AnimationDrawable animation = (AnimationDrawable) characterButton.getBackground();
-            animation.stop();
-            //characterButton.clearAnimation();
-        }
+    private void setShakeAnimation() {
+        ImageView characterButton = getCharacterView();
+        //Animation shakeAnim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.shake);
+        characterButton.startAnimation(shakeAnim);
+    }
+
+    private void setSpeakAnimation() {
+        ImageView characterButton = getCharacterView();
+        characterButton.setBackgroundResource(R.drawable.tesla_speak_anim);
+        AnimationDrawable animation = (AnimationDrawable) characterButton.getBackground();
+        animation.start();
+    }
+
+    private void setCalmAnimation() {
+        ImageView characterButton = getCharacterView();
+        characterButton.setBackgroundResource(R.drawable.tesla);
     }
 
     @Override
@@ -96,12 +132,11 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
                 {
                     setBubbleViewVisibility(false);
                 }
-                else {
+                else
+                {
                     viewFlipper.showNext();
                     setTutorialMenuCallback(viewFlipper.getDisplayedChild());
                 }
-
-
                 break;
 
         }
@@ -121,14 +156,13 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
                 viewFlipper.setDisplayedChild(index);
                 this.setTutorialMenuCallback(index);
             }
-            setShakeAnimation(false);
-            setSpeakAnimation(true);
+            setCharacterAction(CharacterAction.SPEAKING);
         }
         else if (view.getVisibility() == View.VISIBLE && !showBubble){
             view.setVisibility(View.GONE);
             setTutorialMenuCallback(-1);
-            setShakeAnimation(true);
-            setSpeakAnimation(false);
+            setCharacterAction(CharacterAction.CALM);
+
         }
     }
 
@@ -140,14 +174,12 @@ public class TutorialFragment extends Fragment implements View.OnClickListener {
         }
         else
         {
-            //AppLogger.getInstance().d(TAG, "setBubbleCategory with scenarioEnum: " + scenarioEnum);
+            setCharacterAction(CharacterAction.SHAKING);
             ViewFlipper viewFlipper = getViewFlipper();
             viewFlipper.removeAllViews();
             int[] listXmlString = SubjectContent.ENUM_MAP.get(scenarioEnum).getListStringIdTutorial();
-            //AppLogger.getInstance().d(TAG, "setBubbleCategory with scenarioEnum: " + scenarioEnum);
             if (listXmlString != null) {
                 for (int i = 0; i < listXmlString.length; i++) {
-                    //AppLogger.getInstance().d(TAG, "Removing view :" + i);
                     TextView textView = new TextView(this.getActivity());
                     textView.setText(this.getActivity().getText(listXmlString[i]));
                     textView.setTextColor(Color.BLACK);
