7a056c14c1d07047ab76fa635476642d7c563ede
Alexandre Hamel <alexandre.s.hamel@usherbrooke.ca>
2014-11-19 15:30:51 -0500

    Develop : fixing the spotlights.

--- AaltusCode/TeslaRadio/src/com/aaltus/teslaradio/world/Scenarios/ScenarioManager.java
+++ AaltusCode/TeslaRadio/src/com/aaltus/teslaradio/world/Scenarios/ScenarioManager.java
@@ -292,7 +292,7 @@ public class ScenarioManager extends AbstractAppState implements IScenarioManage
     public void setTutorialIndex(int index){
 
         AppLogger.getInstance().d(TAG,"ScenarioManager Index: " + index);
-        ((Scenario) this.getCurrentScenario().getScenarios().get(0)).setCurrentObjectEmphasis(index);
+        ((Scenario) this.getCurrentScenario().getScenarios().get(0).getChild(0)).setCurrentObjectEmphasis(index);
 
     }
 
