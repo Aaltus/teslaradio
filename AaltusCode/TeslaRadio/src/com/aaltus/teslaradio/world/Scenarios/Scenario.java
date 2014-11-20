21d07d9bd4b138a5604e08049867c795799f3c9f
Jean-Christophe Lavoie <jean-christophe.lavoie@usherbrooke.ca>
2014-11-20 13:15:10 -0500

    develop: Shrank model as they are way too big with the new trackables

--- AaltusCode/TeslaRadio/src/com/aaltus/teslaradio/world/Scenarios/Scenario.java
+++ AaltusCode/TeslaRadio/src/com/aaltus/teslaradio/world/Scenarios/Scenario.java
@@ -163,6 +163,7 @@ public abstract class Scenario extends Node implements SignalObserver {
         this.destinationHandle = destinationHandle;
         this.setUserData("angleX", 0f);
         this.scenarioCommon = sc;
+        this.setLocalScale(0.75f);
     }
     
     public Scenario(ScenarioCommon sc, com.jme3.renderer.Camera Camera, Spatial destinationHandle, String bgm)
@@ -174,6 +175,7 @@ public abstract class Scenario extends Node implements SignalObserver {
         if(this.backgroundSound != null){
             this.addControl(new SoundControl(this.backgroundSound,false,1));
         }
+        this.setLocalScale(0.75f);
     }
     
  
